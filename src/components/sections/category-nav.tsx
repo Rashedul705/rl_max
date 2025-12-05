90ed4cfeea63999ceaacec41887eb57e575c7656
Rashedul Islam <rashedul.afl@gmail.com>
2025-12-05 22:37:49 +0000

    lower the bottom padding (_for element section_)

--- src/components/sections/category-nav.tsx
+++ src/components/sections/category-nav.tsx
@@ -4,7 +4,7 @@ import { categories } from "@/lib/data";
 
 export function CategoryNav() {
   return (
-    <section id="categories" className="py-12 md:py-16 bg-background">
+    <section id="categories" className="pt-12 pb-6 md:pt-16 md:pb-8 bg-background">
       <div className="container">
         <div className="flex flex-wrap items-center justify-center gap-4">
           {categories.map((category) => (
